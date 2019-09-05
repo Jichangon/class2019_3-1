2f0345678c56c8a9ef582bcd1eb18ca057758eef
Jichangon <wlrhs6331@naver.com>
2019-09-05 16:17:09 +0900

    스타크래프트

---
+++ myWorkspace/uml/src/main/java/kr/ac/daelim/uml/Marin.java
@@ -0,0 +1,22 @@
+package kr.ac.daelim.uml;
+
+public class Marin {
+
+	public String name;
+	public int health;
+	
+	public void move() {
+		System.out.println("유닛이 이동합니다");
+		
+	}
+	
+	public void attack() {
+		System.out.println("유닛이 공격합니다");
+		
+	}
+	public void underAttack(Charactor charactor) {
+		System.out.println("유닛이 공격합니다");
+		
+	}
+	
+}
